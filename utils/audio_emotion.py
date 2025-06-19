1b99f6c76c6b45efac16062e333348ec8933d817
veenasjayanthan <118438898+veenasjayanthan@users.noreply.github.com>
2025-06-19 10:03:17 +0530

    Added audio_emotion.py  module inside utils folder

---
+++ utils/audio_emotion.py
@@ -0,0 +1,18 @@
+from pyAudioAnalysis import audioSegmentation as aS
+from pyAudioAnalysis import audioBasicIO
+import os
+
+def detect_emotion_from_voice(audio_path):
+    try:
+        [Fs, x] = audioBasicIO.read_audio_file(audio_path)
+        segments, classes, accuracy = aS.mt_file_classification(audio_path, 
+                                                                "pyAudioAnalysis/data/svmSpeechEmotion", 
+                                                                "svm", False)
+        if len(classes) > 0:
+            return classes[0]
+        else:
+            return "unknown"
+    except Exception as e:
+        print("Emotion Detection Error:", e)
+        return "unknown"
+
